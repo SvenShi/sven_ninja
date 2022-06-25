e84443638172a1f51e5c4338fab989ee6b0aa427
ashai <ashai.cn@gmail.com>
2022-06-26 02:26:58 +0800

    fix： 修复删除账号bug

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -15,8 +15,10 @@
       <div class="card-footer">
         <el-button auto @click="logout" style="margin-right: 10px;" v-loading.fullscreen.lock="loading">退出登录</el-button>
         <span v-if="showEnableCk" style="display: inline">
-          <el-button v-if="userStatus === 0" style="margin-right: 10px;" type="danger" auto @click="disableCK">禁用</el-button>
-          <el-button v-if="userStatus === 1"  style="margin-right: 10px;" type="success" auto @click="enableCK">启用</el-button>
+          <el-button v-if="userStatus === 0" style="margin-right: 10px;" type="danger" auto
+                     @click="disableCK">禁用</el-button>
+          <el-button v-if="userStatus === 1" style="margin-right: 10px;" type="success" auto
+                     @click="enableCK">启用</el-button>
         </span>
         <el-button type="danger" auto @click="delAccount" v-loading.fullscreen.lock="loading">删除账号</el-button>
       </div>
@@ -94,17 +96,17 @@ export default {
         return
       }
 
-      getInfoAPI().then(res =>{
-        if (res.data && res.data.code === 200){
+      getInfoAPI().then(res => {
+        if (res.data && res.data.code === 200) {
           that.showEnableCk = Boolean(res.data.allowSetStatus)
-        }else {
+        } else {
           ElMessage.error(res.message)
           that.loading = false
           that.logout()
           return
         }
 
-        verifyUser().then(res =>{
+        verifyUser().then(res => {
           if (res.data.code !== 200) {
             if (res.data.code === 555) {
               ElMessage.error('检查环境变量中配置的密钥长度是否为8的倍数')
@@ -115,7 +117,7 @@ export default {
           }
 
           if (eid) {
-            getUserInfoAPI(eid).then(res =>{
+            getUserInfoAPI(eid).then(res => {
               if (res.code === 400) {
                 ElMessage.error(res.message)
                 return
@@ -140,22 +142,25 @@ export default {
       this.$router.push('/login')
     },
     delAccount() {
-      this.loading = true
       const that = this
       ElMessageBox.confirm("确定删除账号", "警告", {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(async () => {
+        that.loading = true
         const eid = localStorage.getItem('eid')
-        const body = await delAccountAPI({eid})
-        if (body.data.code === 200) {
-          ElMessage.success(body.data.msg)
-          that.logout()
-        } else {
-          ElMessage.error(body.data.msg || body.message)
-        }
-        this.loading = false
+        delAccountAPI({eid}).then(res => {
+              if (res.data.code === 200) {
+                ElMessage.success(res.data.msg)
+                that.logout()
+              } else {
+                ElMessage.error(res.data.msg || res.message)
+              }
+              that.loading = false
+            }
+        )
+
       })
     },
     async updateUsername() {
@@ -226,7 +231,7 @@ export default {
             }
             that.getInfo()
           })
-        }else {
+        } else {
           that.loading = false
           that.$router.push('/')
         }
@@ -250,7 +255,7 @@ export default {
             }
             that.getInfo()
           })
-        }else {
+        } else {
           that.loading = false
           that.$router.push('/')
         }
